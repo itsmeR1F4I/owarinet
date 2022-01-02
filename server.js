35ab8ecaf1f6f65087ed90049d8d99c81c65b3bf
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2022-01-02 18:13:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/add',async(req,res)=>{
-	return res.redirect('minecraft://?addExternalServer=Owari%20Network|owari.ddns.net:19139');
+	return res.redirect('minecraft://?addExternalServer=Owari%20Network|owari.ddns.net:19104');
 });
 app.get('/poi',async(req,res)=>{
 	return res.redirect('https://nekopoi.care/app');
